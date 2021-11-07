c738a20e36ab9c7cf340c48ec0a49f665ff9c1f6
Niloy Sikdar <niloysikdar30@gmail.com>
2021-11-08 00:24:52 +0530

    Added Styling for the Layout

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import styles from '../styles/Navbar.module.css';
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
-      <h2>News App using Next.js</h2>
+      <h2 className={styles.title}>News App using Next.js</h2>
     </div>
   );
 };
